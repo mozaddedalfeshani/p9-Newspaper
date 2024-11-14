3e77f54518cb3d7309c1f62cbe1e49e227081322
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2024-11-14 14:04:45 +0600

    Header section iamge and sologan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import logo from "../assets/logo.png";
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex items-center flex-col gap-2 justify-center my-2">
+      <div>
+        <img src={logo} className="w-[471px] h-[60px]"></img>
+      </div>
+      <p className="text-[#706F6F]">Journalism Without Fear or Favour</p>
+    </div>
+  );
 }
--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 export default function HomeLayout() {
-  return <div>HomeLayout</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
